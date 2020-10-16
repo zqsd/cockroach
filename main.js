9035d2f18a00b0e71131a46eea55cc859de3d24c
Clément Wiz <wiz@haxx.es>
2020-10-17 01:34:04 +0200

    Allow for config passed by arguments

--- main.js
+++ main.js
@@ -2,18 +2,17 @@ const fs = require('fs');
 const {Pool} = require('pg');
 const {transaction, TransactionCancelError} = require('./transaction');
 
-function createPool() {
-    const config = {
-        host: process.env.PGHOST || '127.0.0.1',
-        port: process.env.PGPORT || 26257,
-        user: process.env.PGUSER,
-        database: process.env.PGDATABASE,
-    };
-    if (process.env.PGSSLROOTCERT && process.env.PGSSLKEY && process.env.PGSSLCERT) {
+function createPool(config = {}) {
+    config.host = config.host || process.env.PGHOST || '127.0.0.1';
+    config.port = config.port || process.env.PGPORT || 26257;
+    config.user = config.user || process.env.PGUSER;
+    config.database = config.database || process.env.PGDATABASE;
+    if (process.env.PGSSLROOTCERT && process.env.PGSSLKEY && process.env.PGSSLCERT ||
+        config?.ssl?.ca && config?.ssl?.key && config?.ssl?.cert) {
         config.ssl = {
-            ca: fs.readFileSync(process.env.PGSSLROOTCERT).toString(),
-            key: fs.readFileSync(process.env.PGSSLKEY).toString(),
-            cert: fs.readFileSync(process.env.PGSSLCERT).toString(),
+            ca: fs.readFileSync(process.env.PGSSLROOTCERT || config.ssl.ca).toString(),
+            key: fs.readFileSync(process.env.PGSSLKEY || config.ssl.key).toString(),
+            cert: fs.readFileSync(process.env.PGSSLCERT || config.ssl.cert).toString(),
         };
     }
 
