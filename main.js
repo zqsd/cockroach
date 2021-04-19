9061138a699c7146e30dbedd48b45cad3d433203
Clément <wiz@haxx.es>
2021-04-19 21:37:43 +0200

    Fix config by argument

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ const {Pool} = require('pg');
 const path = require('path');
 const {transaction, TransactionCancelError} = require('./transaction');
 
-function createPool() {
-    const config = {
-        host: config.host || process.env.COCKROACH_HOST || '127.0.0.1',
-        port: config.port || process.env.COCKROACH_PORT || 26257,
-        user: config.user || process.env.COCKROACH_USER || 'root',
-        database: config.database || process.env.COCKROACH_DATABASE || 'defaultdb',
-    };
+function createPool(config) {
+    config.host = config.host || process.env.COCKROACH_HOST || '127.0.0.1';
+    config.port = config.port || process.env.COCKROACH_PORT || 26257;
+    config.user = config.user || process.env.COCKROACH_USER || 'root';
+    config.database = config.database || process.env.COCKROACH_DATABASE || 'defaultdb';
     const insecure = config.insecure || process.env.COCKROACH_INSECURE ? process.env.COCKROACH_INSECURE == 'true' : true;
+
     if((process.env.COCKROACH_CERTS_DIR || config.certsDir) && !insecure) {
         const certsDir = config.certsDir || process.env.COCKROACH_CERTS_DIR;
         config.ssl = {
